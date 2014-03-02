2bdb53a5f906f48181cab44344a8ab2611cdf9d0
hoasung01 <nguyenngochai.shipagent@gmail.com>
2014-03-02 22:57:49 +0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChaCaNhaTrang::Application.config.secret_token = 'bd4e54ee475c2c03b692173f4393994ef9a46492942b57157cfbf9603cf008fdc753666c29b1d812e88b09f831025bba87ff12da7e2fa1f113609f7e894a69b0'
