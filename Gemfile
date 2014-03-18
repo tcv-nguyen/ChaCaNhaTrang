11bf1b6c2363d789e1414a95605550104547a927
hoasung01 <nguyenngochai.shipagent@gmail.com>
2014-03-19 06:01:46 +0700

    added active admin gem

--- Gemfile
+++ Gemfile
@@ -1,5 +1,7 @@
 source 'https://rubygems.org'
 
+
+gem 'activeadmin'
 gem 'rails_12factor'
 gem 'contact_us'
 #gem "formtastic"
@@ -36,7 +38,7 @@ group :production do
   gem "pg"
 end
 
-gem 'jquery-rails'
+gem 'jquery-rails' , "2.3.0"
 
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 ChaCaNhaTrang::Application.routes.draw do
+  root :to => 'welcome#index'
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+
   get "welcome/index"
 
   # The priority is based upon order of creation:
@@ -50,7 +54,6 @@ ChaCaNhaTrang::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
